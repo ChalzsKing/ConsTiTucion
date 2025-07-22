b3d98b1027a69e440d1f5bef3829f69e4bfa2657
chalzsking <chalzsking@gmail.com>
2025-07-22 18:16:18 +0200

    feat: Añadir script de build y configuración final para Vercel

---
+++ Desktop/alejandría_-generador-de-cursos-con-ia (4)/vercel-build.sh
@@ -0,0 +1,16 @@
+C:\Users\PcVip\Desktop\alejandría_-generador-de-cursos-con-ia (4)>vercel inspect alejandria-iwofql908-chalzskings-projects.vercel.app --logs
+Vercel CLI 44.4.1
+2025-07-22T16:06:00.209Z  Running build in Washington, D.C., USA (East) – iad1
+2025-07-22T16:06:00.210Z  Build machine configuration: 2 cores, 8 GB
+2025-07-22T16:06:00.240Z  Retrieving list of deployment files...
+2025-07-22T16:06:00.351Z  Previous build caches not available
+2025-07-22T16:06:00.598Z  Downloading 24 deployment files...
+2025-07-22T16:06:03.072Z  Running "vercel build"
+2025-07-22T16:06:03.559Z  Vercel CLI 44.5.0
+2025-07-22T16:06:04.185Z  Running "install" command: `echo 'Skipping install step since this project uses importmaps.'`...
+2025-07-22T16:06:04.189Z  Skipping install step since this project uses importmaps.
+2025-07-22T16:06:04.295Z  bash: vercel-build.sh: No such file or directory
+2025-07-22T16:06:04.298Z  Error: Command "bash vercel-build.sh" exited with 127
+2025-07-22T16:06:04.499Z
+2025-07-22T16:06:07.350Z  Exiting build container
+status  ● Error
